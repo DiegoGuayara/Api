56e096406610c06fc9cce15a0a1e4b35b91f0017
DiegoGuayara <diegofguayara9707@gmail.com>
2024-09-17 00:01:43 -0500

    Cambios en el index

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const App = express();
 
-App.get("/", (req, res) => res.send("Hola como estan odkweodkewdkwp"));
+App.get("/", (req, res) => res.send("Hola como estan"));
 
 App.listen(3000);
