b737d028d1284cf462e975ba0cd95ee41a0e64c0
DiegoGuayara <diegofguayara9707@gmail.com>
2024-09-17 09:24:20 -0500

    hola

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
-
 const App = express();
 
-App.get("/", (req, res) => res.send("Hola como estan"));
+App.use(express.json())
+
+let usuarios = [
+    {id:1, nombre:'Diana'},
+    {id:2, nombre:'Carlos'},
+    {id:3, nombre:'Ana'}
+]
+
+App.get('/usuarios', (req, res) => {
+    res.json(usuarios)
+})
 
 App.listen(3000);
