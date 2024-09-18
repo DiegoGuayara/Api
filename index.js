758b91887195011b609d8f04b382e6ce7fd17cf2
DiegoGuayara <diegofguayara9707@gmail.com>
2024-09-17 22:40:24 -0500

    ultimos cambios

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 import express from "express";
-const app = express()
-const port = 3000
-app.use(express.json())
+const app = express();
+const port = 3000;
+app.use(express.json());
 
 let usuarios = [
-    {id:1 , nombre: 'ana' , edad : 21},
-    {id:2 , nombre: 'laura', edad: 18}
-]
+  { id: 1, nombre: "ana", edad: 21 },
+  { id: 2, nombre: "laura", edad: 18 },
+];
 
 let miembros = [
-    {id:1 , nombre:'Samuel Andres Marín'},
-    {id:2 , nombre:'Diego Fernando Guayara'}
-]
+  { id: 1, nombre: "Samuel Andres Marín" },
+  { id: 2, nombre: "Diego Fernando Guayara" },
+];
 
-app.get("/usuarios" , (req,res) =>{
-    res.json(usuarios)
-})
-app.get('/usuarios/:id',(req,res) =>{
-    const verificar = parseInt(req.params.id)
-    const verificar2 = usuarios.find(u => u.id === verificar)
-    return verificar2 ? res.json(verificar2) :  res.status(404).send('Usuario no encontrado')
-})
-app.post('/usuarios',(req,res) =>{
-    const nuevoUsuario = req.body
-    nuevoUsuario.id = usuarios.length ? Math.max(...usuarios.map(u => u.id)) + 1 : 1
-    usuarios.push(nuevoUsuario)
-    res.status(201).send(JSON.stringify(nuevoUsuario))
-})
-app.put('/usuarios/:id',(req,res) =>{
-    const nuevo = parseInt(req.params.id)
-    const cuerpo = req.body
-    let user = usuarios.find(u => u.id === nuevo)
-    if(user){
-        user.nombre = cuerpo.nombre
-        res.json(user)
-    }else{
-        res.status(404).send('Usuario no encontrado')
-    }
-})
-app.get('/miembros', (req, res) => {
-    res.json(miembros)
-})
-app.listen(port,() =>{console.log("escuchando en el puerto 3000")})
+app.get("/usuarios", (req, res) => {
+  res.json(usuarios);
+});
+app.get("/miembros", (req, res) => {
+  res.json(miembros);
+});
+app.get("/usuarios/:id", (req, res) => {
+  const verificar = parseInt(req.params.id);
+  const verificar2 = usuarios.find((u) => u.id === verificar);
+  return verificar2
+    ? res.json(verificar2)
+    : res.status(404).send("Usuario no encontrado");
+});
+app.post("/usuarios", (req, res) => {
+  const nuevoUsuario = req.body;
+  nuevoUsuario.id = usuarios.length
+    ? Math.max(...usuarios.map((u) => u.id)) + 1
+    : 1;
+  usuarios.push(nuevoUsuario);
+  res.status(201).send(JSON.stringify(nuevoUsuario));
+});
+app.put("/usuarios/:id", (req, res) => {
+  const nuevo = parseInt(req.params.id);
+  const cuerpo = req.body;
+  let user = usuarios.find((u) => u.id === nuevo);
+  if (user) {
+    user.nombre = cuerpo.nombre;
+    res.json(user);
+  } else {
+    res.status(404).send("Usuario no encontrado");
+  }
+});
+
+app.listen(port, () => {
+  console.log("escuchando en el puerto 3000");
+});
